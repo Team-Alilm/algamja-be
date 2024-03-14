ac5ecff2045b4d17e26a71cc7ef840c575c2f9f1
cloudwi <86584887+cloudwi@users.noreply.github.com>
2024-03-14 10:29:24 +0900

    feat : default time utc

--- src/main/kotlin/org/teamalilm/alilmbe/global/entity/BaseTimeEntity.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/global/entity/BaseTimeEntity.kt
@@ -9,28 +9,31 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset.UTC
+import java.time.ZonedDateTime
+import java.util.TimeZone
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 abstract class BaseTimeEntity(
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    var createdDate: LocalDateTime? = null,
+    var createdDate: ZonedDateTime = ZonedDateTime.of(LocalDateTime.now(), UTC),
 
     @LastModifiedDate
     @Column(nullable = false)
-    var lastModifiedDate: LocalDateTime? = null
+    var lastModifiedDate: ZonedDateTime = ZonedDateTime.of(LocalDateTime.now(), UTC)
 ) {
 
     @PrePersist
     private fun PrePersist() {
-        this.createdDate = LocalDateTime.now()
+        this.createdDate = ZonedDateTime.of(LocalDateTime.now(), UTC)
     }
 
     @PreUpdate
     private fun preUpdate() {
-        this.lastModifiedDate = LocalDateTime.now()
+        this.lastModifiedDate = ZonedDateTime.of(LocalDateTime.now(), UTC)
     }
-
 
 }
