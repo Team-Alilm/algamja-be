04e064ee1baae13079b1c77561bd6301fb72dbe9
cloudwi <cloudwiiiii@gmail.com>
2024-03-15 10:57:29 +0900

    feat : jpa ZonedDateTime 문제 해결

--- src/main/kotlin/org/teamalilm/alilmbe/global/base/BaseTimeEntity.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/global/base/BaseTimeEntity.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import jakarta.persistence.PrePersist
 import jakarta.persistence.PreUpdate
+import java.time.LocalDateTime
 import java.time.ZoneOffset.UTC
 import java.time.ZonedDateTime
 import org.springframework.data.annotation.CreatedDate
@@ -17,22 +18,22 @@ abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    lateinit var createdDate: ZonedDateTime
+    lateinit var createdDate: LocalDateTime
 
     @LastModifiedDate
     @Column(nullable = false)
-    lateinit var lastModifiedDate: ZonedDateTime
+    lateinit var lastModifiedDate: LocalDateTime
 
     @PrePersist
     fun prePersist() {
-        val now = ZonedDateTime.now(UTC)
+        val now = ZonedDateTime.now(UTC).toLocalDateTime()
         createdDate = now
         lastModifiedDate = now
     }
 
     @PreUpdate
     fun preUpdate() {
-        lastModifiedDate = ZonedDateTime.now(UTC)
+        lastModifiedDate = ZonedDateTime.now(UTC).toLocalDateTime()
     }
-    
+
 }
