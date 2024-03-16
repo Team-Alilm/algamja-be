68b43a58eea10e35f31337c49df60b35438e30b5
cloudwi <cloudwiiiii@gmail.com>
2024-03-17 01:12:16 +0900

    fix : cors 필터 origins ch

--- src/main/kotlin/org/teamalilm/alilmbe/global/security/config/WebMvcConfig.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/global/security/config/WebMvcConfig.kt
@@ -9,7 +9,12 @@ class WebMvcConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080", "http://localhost:3000")
+            .allowedOrigins(
+                "http://localhost:8080",
+                "http://localhost:3000",
+                "http://alilm.store",
+                "https://alilm.co.kr"
+            )
             .allowedHeaders("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowCredentials(false)
