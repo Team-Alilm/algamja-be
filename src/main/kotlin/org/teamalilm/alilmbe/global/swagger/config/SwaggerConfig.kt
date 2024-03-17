38a88cba6c3c86936618129e2bae901793ae586b
cloudwi <cloudwiiiii@gmail.com>
2024-03-17 18:22:19 +0900

    fix : cors 필터 origins ch

--- src/main/kotlin/org/teamalilm/alilmbe/global/swagger/config/SwaggerConfig.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/global/swagger/config/SwaggerConfig.kt
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
-import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -38,11 +37,6 @@ class SwaggerConfig {
         return OpenAPI()
             .addSecurityItem(securityRequirement)
             .components(components)
-            .servers(
-                listOf(
-                    Server().url("/").description("prod Server"),
-                )
-            )
     }
 
     companion object {
