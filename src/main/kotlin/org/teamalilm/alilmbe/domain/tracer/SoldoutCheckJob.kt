6fc48189beda562ef2dc5114e8d32cde83986ffc
cloudwi <cloudwiiiii@gmail.com>
2024-03-21 10:29:14 +0900

    feat : 스케줄러 테스트 중

--- src/main/kotlin/org/teamalilm/alilmbe/domain/tracer/SoldoutCheckJob.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/domain/tracer/SoldoutCheckJob.kt
@@ -11,6 +11,13 @@ import org.springframework.web.client.body
 import org.teamalilm.alilmbe.domain.basket.repository.BasketRepository
 import org.teamalilm.alilmbe.domain.product.repository.ProductRepository
 
+/**
+ *  SoldoutCheckJob
+ *
+ *  @author SkyLabs
+ *  @version 1.0.0
+ *  @date 2024-03-21
+ **/
 @Component
 class SoldoutCheckJob(
     val productRepository: ProductRepository,
@@ -28,28 +35,32 @@ class SoldoutCheckJob(
 
         products.forEach {
             val apiUrl = SoldoutScheduler.API_URL_TEMPLATE.format(it.productInfo.number)
+            log.info("무신사 api에 요청을 보냈어요. URL: $apiUrl")
             val response = restClient.get()
                 .uri(URI.create(apiUrl))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body<SoldoutScheduler.SoldoutCheckResponse>()
 
-            log.error("무신사 api의 요청이 성공하지 못했어요. URL: $apiUrl, 상품번호: ${it.productInfo.number}")
+            log.info("무신사 api로부터 응답을 받았어요. 응답: $response")
+            log.info("무신사 api로부터 응답을 받았어요. 응답: ${response?.data?.basic?.get(0)?.name}")
 
             val option1 = it.productInfo.option1
             val option2 = it.productInfo.option2
-
-            val isSoldOut = response?.data?.basic?.firstOrNull { it.name == option1 }?.run {
-                if (subOptions.isNotEmpty()) {
-                    subOptions.find { it.name == option2 }?.run {
-                        isSoldOut
+            log.info("option1: $option1")
+            val isSoldOut =
+                response?.data?.basic?.firstOrNull { item -> item.name == option1 }
+                    ?.run {
+                        if (subOptions.isNotEmpty()) {
+                            subOptions.find { subOption -> subOption.name == option2 }?.run {
+                                isSoldOut
+                            }
+                                ?: throw IllegalStateException("상품 옵션2을 찾지 못했어요. 상품번호: ${it.productInfo.number} 옵션1: $option1")
+                        } else {
+                            isSoldOut
+                        }
                     }
-                        ?: throw IllegalStateException("상품 옵션2을 찾지 못했어요. 상품번호: ${it.productInfo.number} 옵션1: $option1")
-                } else {
-                    isSoldOut
-                }
-            }
-                ?: throw IllegalStateException("상품 옵션1을 찾지 못했어요. 상품번호: ${it.productInfo.number} 옵션1: $option1")
+                    ?: throw IllegalStateException("상품 옵션1을 찾지 못했어요. 상품번호: ${it.productInfo.number} 옵션1: $option1")
 
             if (!isSoldOut) {
                 log.info("상품이 품절되지 않았어요. 상품번호: ${it.productInfo.number}")
