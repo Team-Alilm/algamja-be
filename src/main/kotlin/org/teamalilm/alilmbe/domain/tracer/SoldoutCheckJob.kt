a31268adc82f564d0447f5919cdd42a65c6d5d05
cloudwi <86584887+cloudwi@users.noreply.github.com>
2024-03-21 15:10:50 +0900

    feat : 스케줄러 완료

--- src/main/kotlin/org/teamalilm/alilmbe/domain/tracer/SoldoutCheckJob.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/domain/tracer/SoldoutCheckJob.kt
@@ -35,19 +35,16 @@ class SoldoutCheckJob(
 
         products.forEach {
             val apiUrl = SoldoutScheduler.API_URL_TEMPLATE.format(it.productInfo.number)
-            log.info("무신사 api에 요청을 보냈어요. URL: $apiUrl")
             val response = restClient.get()
                 .uri(URI.create(apiUrl))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body<SoldoutScheduler.SoldoutCheckResponse>()
 
-            log.info("무신사 api로부터 응답을 받았어요. 응답: $response")
-            log.info("무신사 api로부터 응답을 받았어요. 응답: ${response?.data?.basic?.get(0)?.name}")
 
             val option1 = it.productInfo.option1
             val option2 = it.productInfo.option2
-            log.info("option1: $option1")
+
             val isSoldOut =
                 response?.data?.basic?.firstOrNull { item -> item.name == option1 }
                     ?.run {
@@ -63,7 +60,6 @@ class SoldoutCheckJob(
                     ?: throw IllegalStateException("상품 옵션1을 찾지 못했어요. 상품번호: ${it.productInfo.number} 옵션1: $option1")
 
             if (!isSoldOut) {
-                log.info("상품이 품절되지 않았어요. 상품번호: ${it.productInfo.number}")
                 soldoutProductIds.add(it.id!!)
             }
         }
