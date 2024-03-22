63eb74d35411471d15b08a5c7e80d0c32b03a380
cloudwi <cloudwiiiii@gmail.com>
2024-03-22 15:50:12 +0900

    feat : 메일 시스템 개발

--- src/main/kotlin/org/teamalilm/alilmbe/global/email/data/EmailMessage.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/global/email/data/EmailMessage.kt
@@ -1,6 +1,7 @@
 package org.teamalilm.alilmbe.global.email.data
 
 data class EmailMessage(
+    val from: String = "Team Alilm",
     val to: String,
     val subject: String,
     val text: String
