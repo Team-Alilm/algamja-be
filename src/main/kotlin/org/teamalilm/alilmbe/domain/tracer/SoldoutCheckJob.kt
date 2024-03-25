1afdb3129804ca812ff9d9abf32b6185e872af48
cloudwi <cloudwiiiii@gmail.com>
2024-03-25 13:45:00 +0900

    feat : slack 연동

--- src/main/kotlin/org/teamalilm/alilmbe/domain/tracer/SoldoutCheckJob.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/domain/tracer/SoldoutCheckJob.kt
@@ -6,47 +6,52 @@ import org.quartz.JobExecutionContext
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
 import org.teamalilm.alilmbe.domain.basket.repository.BasketRepository
-import org.teamalilm.alilmbe.domain.product.repository.ProductRepository
 import org.teamalilm.alilmbe.global.email.data.EmailMessage
 import org.teamalilm.alilmbe.global.email.service.EmailService
+import org.teamalilm.alilmbe.global.slack.service.SlackService
 
 /**
  *  SoldoutCheckJob
  *
- *  @author SkyLabs
+ *  @author jubi
  *  @version 1.0.0
  *  @date 2024-03-21
  **/
 @Component
+@Transactional(readOnly = true)
 class SoldoutCheckJob(
-    val productRepository: ProductRepository,
     val basketRepository: BasketRepository,
-    val emailService: EmailService
-
+    val emailService: EmailService,
+    val slackService: SlackService
 ) : Job {
 
     private val log = LoggerFactory.getLogger(SoldoutCheckJob::class.java)
+
+    @Transactional
     override fun execute(context: JobExecutionContext) {
         log.info("SoldoutCheckJob is running")
 
-        val soldoutProductIds = mutableListOf<Long>()
+        val restockProductIds = mutableListOf<Long>()
         val restClient = RestClient.create()
-        val products = productRepository.findAllByOrderByCreatedDateDesc()
+        val baskets = basketRepository.findAllByGroupByProductId()
 
-        products.forEach {
-            val apiUrl = SoldoutScheduler.API_URL_TEMPLATE.format(it.productInfo.number)
+        baskets.forEach {
+            log.info("상품 ID: ${it.product.id}")
+
+            val apiUrl = SoldoutScheduler.API_URL_TEMPLATE.format(it.product.productInfo.number)
+
             val response = restClient.get()
                 .uri(URI.create(apiUrl))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .body<SoldoutScheduler.SoldoutCheckResponse>()
 
-
-            val option1 = it.productInfo.option1
-            val option2 = it.productInfo.option2
+            val option1 = it.product.productInfo.option1
+            val option2 = it.product.productInfo.option2
 
             val isSoldOut =
                 response?.data?.basic?.firstOrNull { item -> item.name == option1 }
@@ -55,29 +60,33 @@ class SoldoutCheckJob(
                             subOptions.find { subOption -> subOption.name == option2 }?.run {
                                 isSoldOut
                             }
-                                ?: throw IllegalStateException("상품 옵션2을 찾지 못했어요. 상품번호: ${it.productInfo.number} 옵션1: $option1")
+                                ?: throw IllegalStateException("상품 옵션2을 찾지 못했어요. 상품번호: ${it.product.productInfo.number} 옵션1: $option1")
                         } else {
                             isSoldOut
                         }
                     }
-                    ?: throw IllegalStateException("상품 옵션1을 찾지 못했어요. 상품번호: ${it.productInfo.number} 옵션1: $option1")
+                    ?: throw IllegalStateException("상품 옵션1을 찾지 못했어요. 상품번호: ${it.product.productInfo.number} 옵션1: $option1")
 
             if (!isSoldOut) {
-                soldoutProductIds.add(it.id!!)
+                restockProductIds.add(it.id!!)
             }
         }
 
-        if (soldoutProductIds.isNotEmpty()) {
+        restockProductIds.forEach {
+            log.info("재입고 상품 ID: $it")
+
+            val basket = basketRepository.findByProductId(it)
+                ?: throw IllegalStateException("해당 상품을 찾을 수 없어요. 상품 ID: $it")
+
             val emailMessage = EmailMessage(
-                from = "cloudwi@naver.com",
+                from = "Alilm",
                 to = "cloudwi@naver.com",
-                subject = "품절 상품 알림",
-                text = "품절 상품이 있습니다. 확인해주세요.",
+                subject = "재입고 알림",
+                text = "${basket.product.name} 상품이 재입고 되었습니다. 확인해주세요.",
             )
-
+            basketRepository.deleteById(it)
             emailService.sendMail(emailMessage)
+            slackService.sendSlackMessage("${basket.product.name} 상품이 재입고 되었습니다. 확인해주세요.")
         }
-
-        basketRepository.deleteByProductIds(soldoutProductIds)
     }
 }
