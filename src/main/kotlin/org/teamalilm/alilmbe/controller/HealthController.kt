e3a5400ba0927c27ae742929adca4eb2cfb517ad
cloudwi <86584887+cloudwi@users.noreply.github.com>
2024-04-09 08:54:06 +0900

    feat : 배포 재 설정

--- src/main/kotlin/org/teamalilm/alilmbe/controller/HealthController.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/controller/HealthController.kt
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/health")
 class HealthController {
 
-    @GetMapping
+    @GetMapping("/health-check")
     fun health() : String {
         return "Hello Alilm"
     }
