321eee0aea1683b9e388dc32f5eb49d2243c5132
jangjuyeong <cloudwi@naver.com>
2024-04-10 03:22:15 +0900

    feat : 배포 경로 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ec2-user/app/deploy"
-JAR_FILE="$PROJECT_ROOT/build/libs/Alilm-Be-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/Alilm-Be-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
