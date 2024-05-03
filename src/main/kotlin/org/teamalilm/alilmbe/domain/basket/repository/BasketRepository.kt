5e6df9c9f31373527283a548d56bfd6270484730
cloudwi <cloudwiiiii@gmail.com>
2024-05-03 11:40:28 +0900

    feat : 전체 조회 api 추가했어요.

--- src/main/kotlin/org/teamalilm/alilmbe/domain/basket/repository/BasketRepository.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/domain/basket/repository/BasketRepository.kt
@@ -1,25 +1,21 @@
 package org.teamalilm.alilmbe.domain.basket.repository
 
-import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.teamalilm.alilmbe.domain.basket.entity.Basket
-import org.teamalilm.alilmbe.domain.member.entity.Member
-import org.teamalilm.alilmbe.domain.product.entity.Product
 
 interface BasketRepository : JpaRepository<Basket, Long> {
 
-    @Query(
-        "SELECT COUNT(b), b.product, b.member " +
-                "FROM Basket b " +
-                "GROUP BY b.product"
-    )
-    fun findAll(pageRequest: PageRequest): Slice<BasketFindAllQuery>
+    @Query("SELECT b as basket, (SELECT COUNT(b2) FROM Basket b2 WHERE b2.product.id = b.product.id) as count " +
+            "FROM Basket b " +
+            "WHERE b.id " +
+            "IN (SELECT MIN(b3.id) FROM Basket b3 GROUP BY b3.product.id)")
+    fun findDistinctByProductAndOldestCreationTimeWithCount(pageable: Pageable): Slice<BasketCountProjection>
 
-    data class BasketFindAllQuery(
-        val count: Long,
-        val product: Product,
-        val member: Member
-    )
+    interface BasketCountProjection {
+        fun getBasket(): Basket
+        fun getCount(): Long
+    }
 }
