5f0436245035254b07818f0f8bf670c79876f127
cloudwi <cloudwiiiii@gmail.com>
2024-05-05 13:40:14 +0900

    feat : 헬스 체크 api 스웨거에 공유해요.

--- src/main/kotlin/org/teamalilm/alilmbe/controller/HealthController.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/controller/HealthController.kt
@@ -1,14 +1,17 @@
 package org.teamalilm.alilmbe.controller
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Tag(name = "health", description = "Health Check API")
 class HealthController {
 
     @GetMapping("/health-check")
-    fun health() : String {
+    @Operation(summary = "Health Check API", description = "Health Check API")
+    fun health(): String {
         return "Hello Alilm"
     }
 
