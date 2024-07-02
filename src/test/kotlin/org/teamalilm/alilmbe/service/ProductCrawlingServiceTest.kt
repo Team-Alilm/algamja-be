613728657b6b56e5d16e8749524efc1b2e6479e0
cloudwi <86584887+cloudwi@users.noreply.github.com>
2024-07-03 08:27:23 +0900

    feat : 크롤링하는 api를 완성했어요.

--- src/test/kotlin/org/teamalilm/alilmbe/service/ProductCrawlingServiceTest.kt
+++ src/test/kotlin/org/teamalilm/alilmbe/service/ProductCrawlingServiceTest.kt
@@ -1,5 +1,6 @@
 package org.teamalilm.alilmbe.service
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 import org.teamalilm.alilmbe.service.crawling.ProductCrawlingService
@@ -10,12 +11,15 @@ class ProductCrawlingServiceTest {
 
     @Test
     fun crawling() {
+        // given
         val command = ProductCrawlingService.ProductCrawlingCommand(
             url = "https://www.musinsa.com/app/goods/3262292"
         )
 
+        // when
         val result = productCrawlingService.crawling(command)
 
-        println(result)
+        // then
+        Assertions.assertThat(result.name).isEqualTo("오버사이즈 립 포켓 하프 셔츠 블랙")
     }
 }
