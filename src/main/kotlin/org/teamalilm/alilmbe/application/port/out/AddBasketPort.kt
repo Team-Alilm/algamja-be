a62a9776ce5407c21e0b5dea218f82b590ddbc28
장주영 <jooyoung.jang@skylabs.io>
2024-08-01 11:18:17 +0900

    feat: 최적화 했어요.

--- src/main/kotlin/org/teamalilm/alilmbe/application/port/out/AddBasketPort.kt
+++ src/main/kotlin/org/teamalilm/alilmbe/application/port/out/AddBasketPort.kt
@@ -1,6 +1,5 @@
 package org.teamalilm.alilmbe.application.port.out
 
-import org.teamalilm.alilmbe.adapter.out.persistence.entity.MemberJpaEntity
 import org.teamalilm.alilmbe.domain.Basket
 import org.teamalilm.alilmbe.domain.Member
 import org.teamalilm.alilmbe.domain.Product
