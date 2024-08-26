ddbe84803eb2598da3d175052e57cb780745d543
cloudwi <cloudwiiiii@gmail.com>
2024-08-26 15:26:46 +0900

    feat: 운영 환경에서 쿼리를 보지 않도록 설정 했어요.

--- src/main/kotlin/org/teamalilm/alilm/common/error/ErrorMessage.kt
+++ src/main/kotlin/org/teamalilm/alilm/common/error/ErrorMessage.kt
@@ -8,5 +8,4 @@ enum class ErrorMessage(
     NOT_FOUND_MEMBER("ALILM-003", "회원을 찾지 못했습니다."),
     NOT_FOUND_ROLE("ALILM-004", " 권환을 찾지 못했습니다."),
     NOT_FOUND_PRODUCT("ALILM-005", "상품을 찾지 못했습니다."),
-    NOT_FOUND_BASKET("ALILM-006", "바구니를 찾지 못했습니다."),
 }
