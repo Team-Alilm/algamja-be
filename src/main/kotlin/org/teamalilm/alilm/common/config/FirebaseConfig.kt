3fd5eca108a706df0a36de26feff1f67dbfea8a3
cloudwi <cloudwiiiii@gmail.com>
2024-08-28 16:28:08 +0900

    feat: fcm token을 추가합니다.

---
+++ src/main/kotlin/org/teamalilm/alilm/common/config/FirebaseConfig.kt
@@ -0,0 +1,28 @@
+package org.teamalilm.alilm.common.config
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+
+@Configuration
+class FirebaseConfig {
+
+    @Bean
+    fun firebaseApp(): FirebaseApp {
+        val firebaseSecretKey = ClassPathResource("/firebase/FirebaseSecretKey.json")
+        val firebaseOptions = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(firebaseSecretKey.inputStream))
+            .build()
+
+        return FirebaseApp.initializeApp(firebaseOptions)
+    }
+
+    @Bean
+    fun firebaseMessaging(firebaseApp: FirebaseApp?): FirebaseMessaging {
+        return FirebaseMessaging.getInstance(firebaseApp)
+    }
+}
