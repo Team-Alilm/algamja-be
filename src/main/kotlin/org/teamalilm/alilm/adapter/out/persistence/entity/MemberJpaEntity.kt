5977a9a653d2008f638bb06aab4caa06d12bb876
cloudwi <cloudwiiiii@gmail.com>
2024-09-26 08:22:36 +0900

    fix: fcm token을 null로 초기화 하는 작업은 필요 없겠네요

--- src/main/kotlin/org/teamalilm/alilm/adapter/out/persistence/entity/MemberJpaEntity.kt
+++ src/main/kotlin/org/teamalilm/alilm/adapter/out/persistence/entity/MemberJpaEntity.kt
@@ -1,7 +1,7 @@
 package org.teamalilm.alilm.adapter.out.persistence.entity
 
 import jakarta.persistence.*
-import org.teamalilm.alilm.global.jpa.base.BaseEntity
+import org.teamalilm.alilm.global.jpa.base.BaseTimeEntity
 import org.teamalilm.alilm.global.security.service.oAuth2.data.Provider
 
 @Entity
@@ -29,9 +29,9 @@ class MemberJpaEntity(
     var nickname: String,
 
     @Column
-    var fcmToken: String? = null,
+    var fcmToken: String?,
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
-) : BaseEntity()
+) : BaseTimeEntity()
