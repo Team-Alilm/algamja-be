fad2aa69d545ec5f0d35ca6204932f3628317679
장주영 <jooyoung.jang@skylabs.io>
2024-10-02 08:50:44 +0900

    feat: 이미등록된 상품은 409 code를 반환합니다.

--- src/main/kotlin/org/teamalilm/alilm/adapter/in/web/advice/RestControllerAdvice.kt
+++ src/main/kotlin/org/teamalilm/alilm/adapter/in/web/advice/RestControllerAdvice.kt
@@ -1,15 +1,23 @@
 package org.teamalilm.alilm.adapter.`in`.web.advice
 
 import com.google.api.pathtemplate.ValidationException
+import io.netty.handler.codec.http.HttpResponseStatus.CONFLICT
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.teamalilm.alilm.common.error.BasketAlreadyExistsException
 
 @RestControllerAdvice
 class RestControllerAdvice {
 
     private val log = LoggerFactory.getLogger(RestControllerAdvice::class.java)
+
+    @ExceptionHandler(value = [BasketAlreadyExistsException::class])
+    fun handleBasketAlreadyExistsException(e: BasketAlreadyExistsException): ResponseEntity<String> {
+        return ResponseEntity.status(CONFLICT.code()).body(e.message)
+    }
 
     @ExceptionHandler(value = [ValidationException::class])
     fun handleValidationException(e: ValidationException): ResponseEntity<String> {
