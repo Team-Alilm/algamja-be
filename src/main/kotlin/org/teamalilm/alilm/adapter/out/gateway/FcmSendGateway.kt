b67c7c743305e6a91df6685c280bfb2807feb649
cloudwi <cloudwiiiii@gmail.com>
2024-10-02 11:12:19 +0900

    feat: 푸쉬 알림에 이미지 추가 했어요.

--- src/main/kotlin/org/teamalilm/alilm/adapter/out/gateway/FcmSendGateway.kt
+++ src/main/kotlin/org/teamalilm/alilm/adapter/out/gateway/FcmSendGateway.kt
@@ -25,27 +25,30 @@ class FcmSendGateway(
         val options = listOfNotNull(product.firstOption, product.secondOption, product.thirdOption)
             .joinToString(" / ")
 
-        val imageUrl = "https://alilm.store/alilm.png"
+        val imageUrl = "https://www.alilm.co.kr/icons/alilm.svg?width=100px&height=100px"
 
         // FCM 메시지 구성
-        val message = firebaseMessaging.send(
-            Message.builder()
-                .setNotification(
-                    Notification.builder()
-                        .setTitle("알림 : 등록 하신 상품이 재 입고 되었어요.")
-                        .setBody("""
-                            ${product.name} 상품이 재입고 되었습니다.
-                            ${if (options.isNotBlank()) "option : $options" else ""}
-                           
-                            바로 확인해보세요!
-                            https://www.musinsa.com/products/${product.number}
-                        """.trimIndent())
-                        .setImage(imageUrl)
-                        .build()
+        val message = Message.builder()
+            // Notification 메시지에 이미지 포함
+            .setNotification(
+                Notification.builder()
+                    .setTitle("알림 : 등록 하신 상품이 재 입고 되었어요.")
+                    .setBody("""
+                        ${product.name} 상품이 재입고 되었습니다.
+                        ${if (options.isNotBlank()) "option : $options" else ""}
+                
+                        바로 확인해보세요!
+                    """.trimIndent())
+                    .setImage("https://www.alilm.co.kr/icons/alilm.svg?width=100px&height=100px")  // 이미지 포함
+                    .build()
                 )
-                .setToken(fcmToken.token)  // 실제 FCM 토큰을 설정해야 함
+
+                // 데이터 메시지에 클릭 링크 추가
+                .putData("click_action", "https://www.musinsa.com/products/${product.number}")
+                .setToken(fcmToken.token)
                 .build()
-        )
+
+        firebaseMessaging.send(message)
 
         log.info("Successfully sent message: $message")
     }
--- src/test/kotlin/org/teamalilm/alilm/adapter/out/gateway/FcmSendGatewayTest.kt
+++ src/test/kotlin/org/teamalilm/alilm/adapter/out/gateway/FcmSendGatewayTest.kt
@@ -1,13 +1,13 @@
 package org.teamalilm.alilm.adapter.out.gateway
 
-import com.google.firebase.messaging.Message
-import com.google.firebase.messaging.Notification
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.teamalilm.alilm.domain.FcmToken
+import org.teamalilm.alilm.domain.Member
+import org.teamalilm.alilm.domain.Product
+import org.teamalilm.alilm.global.security.service.oAuth2.data.Provider
 
-@Disabled
 @SpringBootTest
 class FcmSendGatewayTest {
 
@@ -16,22 +16,34 @@ class FcmSendGatewayTest {
 
     @Test
     fun sendFcmMessage() {
-        // 실제 FCM 토큰을 여기에 넣으세요
-        val actualToken = "fpAvwbzb-HNpwBb_odzt9c:APA91bF04uKrm0YYlx5Pj0tnJs_XXAYmPES6XOQrjprHoZpe8T1i0WDAoZxGasgYNH12LZKbCX8OhYTEjyfOb5vyNoHN95Z49FJ4tC0DfH7IjHYt0Ju3n0l8-HVRbQ77uZh1LpHAhJgH";
+        var member = Member(
+            provider = Provider.KAKAO,
+            providerId = 1L,
+            email = "c",
+            nickname = "c",
+        )
 
-        val message = Message.builder()
-            .setNotification(
-                Notification.builder()
-                    .setTitle("테스트 알림")
-                    .setBody("테스트 메시지가 도착했습니다.")
-                    .setImage("https://alilm.store/alilm.png")
-                    .build()
-            )
-            .setToken(actualToken)
-            .build()
+        var product = Product(
+            id = Product.ProductId(1L),
+            number = 1L,
+            name = "c",
+            brand = "c",
+            imageUrl = "c",
+            category = "c",
+            price = 1,
+            store = Product.Store.MUSINSA,
+            firstOption = "c",
+            secondOption = "c",
+            thirdOption = "c"
+        )
 
-//        val response = fcmSendGateway.sendFcmMessage(message)
+        val fcmToken = FcmToken(
+            token = "fm3LCU4VwnHXsbQpCC9_f4:APA91bGEDgfJTgx8PCc1ZKpEBPvkcz9kPVSfOpI9c3X7ZLIPa5ujaf8eMoU0jX3s3EnMI2IEVWHWJkQAhtIqzhaQzlS1JgW5wbftNwvIzBw12JuSkPtwF0PIVxm-_8oaFPgoFR4t19fc",
+            memberId = Member.MemberId(1L)
+        )
 
-//        println("FCM 메시지 전송 결과: $response")
+        val response = fcmSendGateway.sendFcmMessage(member, product ,fcmToken)
+
+        println("FCM 메시지 전송 결과: $response")
     }
 }
