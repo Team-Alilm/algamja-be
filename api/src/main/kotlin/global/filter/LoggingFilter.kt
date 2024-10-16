b1d396b21bb8c3469304c2fdd0527917919f834c
장주영 <jooyoung.jang@skylabs.io>
2024-10-17 08:36:55 +0900

    feat: 사용자 정보 수정 api를 개발 했어요.
    - loggingFilter에서 사용하지 않는 의존성 제거
    - member email 변경 가능한 var 변수로 변경
    - controller, service 제작
    - ouath2 login 시 사용자 정보 변경 없도록 수정

--- api/src/main/kotlin/global/filter/LoggingFilter.kt
+++ api/src/main/kotlin/global/filter/LoggingFilter.kt
@@ -3,8 +3,6 @@ package org.team_alilm.global.filter
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.filter.OncePerRequestFilter
 import org.springframework.web.util.ContentCachingRequestWrapper
 import org.springframework.web.util.ContentCachingResponseWrapper
