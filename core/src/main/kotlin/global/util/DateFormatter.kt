575536385be9ffbdec96f2fc9d683e04778a963d
cloudwi <cloudwiiiii@gmail.com>
2024-10-17 13:47:04 +0900

    fix: 테스트 전으로 복구합니다

--- core/src/main/kotlin/global/util/DateFormatter.kt
+++ core/src/main/kotlin/global/util/DateFormatter.kt
@@ -1,15 +1,10 @@
 package org.team_alilm.global.util
 
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
+import java.text.SimpleDateFormat
 
 object DateFormatter {
 
     fun dateFormatter(date: Long) : String {
-        val localDateTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(date), ZoneId.systemDefault())
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-        return localDateTime.format(formatter)
+        return SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date)
     }
 }
