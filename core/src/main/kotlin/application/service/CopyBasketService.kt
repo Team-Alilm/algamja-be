e41dbbf57bf3b379a8159cb6c143aa053fa3ca03
장주영 <jooyoung.jang@skylabs.io>
2024-10-21 18:31:56 +0900

    feat : 함께 기다리기 이미 등록된 상품이라도 정상적으로 응답 하도록 수정

--- core/src/main/kotlin/application/service/CopyBasketService.kt
+++ core/src/main/kotlin/application/service/CopyBasketService.kt
@@ -3,6 +3,7 @@ package org.team_alilm.application.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.team_alilm.application.port.`in`.use_case.CopyBasketUseCase
+import org.team_alilm.application.port.out.LoadBasketPort
 import org.team_alilm.domain.Basket
 import org.team_alilm.domain.Product
 import org.team_alilm.global.error.ErrorMessage
@@ -12,7 +13,8 @@ import org.team_alilm.global.error.NotFoundProductException
 @Transactional(readOnly = true)
 class CopyBasketService(
     private val loadProductPort: org.team_alilm.application.port.out.LoadProductPort,
-    private val addBasketPort: org.team_alilm.application.port.out.AddBasketPort
+    private val addBasketPort: org.team_alilm.application.port.out.AddBasketPort,
+    private val loadBasketPort: LoadBasketPort
 ) : CopyBasketUseCase {
 
     @Transactional
@@ -24,9 +26,17 @@ class CopyBasketService(
             memberId = command.member.id!!
         )
 
+        // 상품 조회
         val product = loadProductPort.loadProduct(productId = productId)
             ?: throw NotFoundProductException(ErrorMessage.NOT_FOUND_PRODUCT)
 
+        // 이미 장바구니에 담긴 상품인지 확인
+        loadBasketPort.loadBasket(
+            memberId = command.member.id,
+            productId = productId
+        )?.let { return }
+
+        // 장바구니에 상품 추가
         addBasketPort.addBasket(
             basket = basket,
             product = product,
