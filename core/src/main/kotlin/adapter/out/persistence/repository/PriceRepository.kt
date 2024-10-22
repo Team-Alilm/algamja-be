4c0e5c371cdec98218497862793bee62c6493e2c
장주영 <jooyoung.jang@skylabs.io>
2024-10-22 13:08:26 +0900

    feat : 가격 로직 삭제

--- core/src/main/kotlin/adapter/out/persistence/repository/PriceRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.team_alilm.adapter.out.persistence.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.team_alilm.adapter.out.persistence.entity.PriceJpaEntity
-
-interface PriceRepository : JpaRepository<PriceJpaEntity, Long> {
-}
-
