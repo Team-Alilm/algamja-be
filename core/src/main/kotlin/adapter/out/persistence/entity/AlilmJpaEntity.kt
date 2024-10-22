14d55ce5d2efea7c93c9540dbe26c0366fe16479
cloudwi <cloudwiiiii@gmail.com>
2024-10-23 08:29:00 +0900

    feat : 장바구니 등록 시 이미 알림 받은 상품은 다시 알림으로 등록 합니다.

---
+++ core/src/main/kotlin/adapter/out/persistence/entity/AlilmJpaEntity.kt
@@ -0,0 +1,35 @@
+package org.team_alilm.adapter.out.persistence.entity
+
+import jakarta.persistence.*
+import org.team_alilm.global.jpa.base.BaseTimeEntity
+
+@Entity
+@Table(
+    name = "alilm",
+    indexes = [
+        Index(
+            name = "idx_product_id",
+            columnList = "product_id"
+        ),
+        Index(
+            name = "idx_member_id",
+            columnList = "member_id"
+        ),
+        Index(
+            name = "idx_product_member",
+            columnList = "product_id, member_id"
+        )
+    ]
+)
+class AlilmJpaEntity(
+
+    @Column(nullable = false)
+    val productId: Long,
+
+    @Column(nullable = false)
+    val memberId: Long,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+) : BaseTimeEntity()
