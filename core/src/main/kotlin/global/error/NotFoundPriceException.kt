24322a6fecd0ed9b79d0709a8a56a64419a4fde9
장주영 <jooyoung.jang@skylabs.io>
2024-10-22 13:17:05 +0900

    feat : 가격 로직 삭제

--- core/src/main/kotlin/global/error/NotFoundPriceException.kt
+++
@@ -1,9 +0,0 @@
-package org.team_alilm.global.error
-
-class NotFoundPriceException(
-    val errorMessage: ErrorMessage
-) : RuntimeException() {
-
-    override val message: String
-        get() { return "${ErrorMessage.NOT_FOUND_PRODUCT.code} : ${ErrorMessage.NOT_FOUND_PRICE.message}" }
-}
