391a813dda53686039d2670abe2c80dfbf284ab4
cloudwi <cloudwiiiii@gmail.com>
2024-10-24 15:38:56 +0900

    feat : 상품 전체 조회 시 아무도 기다리지 않아도 출력 되도록 left join 적용

--- core/src/main/kotlin/adapter/out/persistence/repository/ProductRepository.kt
+++ core/src/main/kotlin/adapter/out/persistence/repository/ProductRepository.kt
@@ -3,7 +3,6 @@ package org.team_alilm.adapter.out.persistence.repository
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.team_alilm.adapter.out.persistence.entity.ProductJpaEntity
-import org.team_alilm.application.port.out.LoadProductsInBasketsPort
 import org.team_alilm.domain.Product
 
 interface ProductRepository : JpaRepository<ProductJpaEntity, Long> {
@@ -16,15 +15,15 @@ interface ProductRepository : JpaRepository<ProductJpaEntity, Long> {
         thirdOption: String?,
     ): ProductJpaEntity?
 
-    @Query(
-        """
-        select distinct p from ProductJpaEntity p
-        join BasketJpaEntity b
-        on p.id = b.productId
-        where b.isDelete = false
-        and b.isAlilm = false
+    @Query(value = """
+        select p.id, p.number, p.brand, p.image_url, p.store, p.category, p.price, p.first_option, p.second_option, p.third_option, p.is_delete, p.created_date, p.last_modified_date, p.name
+        from product p
+        join basket b
+        on p.id = b.product_id
+        where b.is_delete = false
+        and b.is_alilm = false
         group by p.number
-        """
+        """, nativeQuery = true
     )
     fun findProductsInBaskets(): List<ProductJpaEntity>
 
