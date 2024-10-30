f64afd239d5833f850baaac0a6c04504409d907f
cloudwi <cloudwiiiii@gmail.com>
2024-10-30 10:30:51 +0900

    feat : fcm에 모든 데이터를 data객체에 담아서 보내도록 수정했어요.

--- core/src/main/kotlin/adapter/out/gateway/FcmSendGateway.kt
+++ core/src/main/kotlin/adapter/out/gateway/FcmSendGateway.kt
@@ -2,7 +2,6 @@ package org.team_alilm.adapter.out.gateway
 
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
-import com.google.firebase.messaging.Notification
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.team_alilm.domain.FcmToken
@@ -25,23 +24,17 @@ class FcmSendGateway(
         val options = listOfNotNull(product.firstOption, product.secondOption, product.thirdOption)
             .joinToString(" / ")
 
-        // FCM 메시지 구성
+        // FCM 메시지 구성 (Data 메시지로만)
         val message = Message.builder()
-            // Notification 메시지에 이미지 포함
-            .setNotification(
-                Notification.builder()
-                    .setImage(product.imageUrl)
-                    // [유니폼 브릿지 야상] 상품이 재 입고 되었습니다!
-                    .setTitle("[${product.name}] 상품이 재 입고 되었습니다!")
-                    .setBody("""
-                        ${if (options.isNotBlank()) "option : $options" else ""}
-                        지금 바로 확인해보세요. server에서 발송하는 fcm입니다.
-                    """.trimIndent())
-                    .build()
-                )
-
-                .setToken(fcmToken.token)
-                .build()
+            .putData("title", "[${product.name}] 상품이 재 입고 되었습니다!")
+            .putData("body", """
+                ${if (options.isNotBlank()) "option : $options" else ""}
+                지금 바로 확인해보세요. 서버에서 발송하는 FCM입니다.
+            """.trimIndent())
+            .putData("image", product.imageUrl) // 이미지 URL 추가
+            .putData("click_action", "https://www.musinsa.com/products/${product.number}")
+            .setToken(fcmToken.token)
+            .build()
 
         firebaseMessaging.send(message)
 
