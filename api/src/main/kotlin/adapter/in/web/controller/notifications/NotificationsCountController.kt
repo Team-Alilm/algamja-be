afde55736daab34cc515024670b61fbf8872415d
cloudwi <cloudwiiiii@gmail.com>
2024-11-01 08:22:06 +0900

    feat : 전체 갯수반 환을 alilm 테이블에서 진행하는 것으로 수정했어요

--- api/src/main/kotlin/adapter/in/web/controller/notifications/NotificationsCountController.kt
+++ api/src/main/kotlin/adapter/in/web/controller/notifications/NotificationsCountController.kt
@@ -9,19 +9,25 @@ import org.springframework.web.bind.annotation.RestController
 import org.team_alilm.application.port.`in`.use_case.AlilmHistoryUseCase
 
 @RestController
-@RequestMapping("/api/v1/notifications")
-@Tag(name = "알림 히스토리 API", description = "알림 히스토리 관련 API")
+@RequestMapping("/api/v1/notifications", "/api/v1/alilms")
+@Tag(name = "알림 히스토리 API", description = """
+    알림 히스토리 관련 API
+    alilm 건 수를 반환하는 API 입니다.
+    
+    현재 2개의 주소로 운영되고 있으며
+    향후 /api/v1/notifications는 사용을 종료 합니다.
+    """)
 class NotificationsCountController(
     private val alilmHistoryUseCase: AlilmHistoryUseCase
 ) {
-
-    // todo alilm에 대한 로직이 복잡해 지면 테이블을 분리하고 고도화 작업을 진행하면 좋을 것 같습니다.
     // 현재는 장바구니 테이블에 의존적으로 개발 합니다.
     @Operation(
         summary = "알림 기록 조회 API",
         description = """
             실질적으로 장바구니에서 카운팅 해서 내려가는 데이터 입니다.
             장바구니에 대한 알림을 조회합니다.
+            
+            한국 시간으로 운영되고 있습니다.
     """)
     @GetMapping("/count")
     fun alilmHistory(): ResponseEntity<AlilmHistoryResponse> {
