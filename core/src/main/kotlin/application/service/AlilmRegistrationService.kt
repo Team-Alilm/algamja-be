e39285f203858f8c131d19d646fa3687e57a6b59
cloudwi <cloudwiiiii@gmail.com>
2024-11-04 10:23:45 +0900

    feat : 이제 사용자가 장바구니를 등록하면 저희에게 알림이 날라와요.

--- core/src/main/kotlin/application/service/AlilmRegistrationService.kt
+++ core/src/main/kotlin/application/service/AlilmRegistrationService.kt
@@ -2,10 +2,12 @@ package org.team_alilm.application.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.team_alilm.adapter.out.gateway.SlackGateway
 import org.team_alilm.application.port.`in`.use_case.AlilmRegistrationUseCase.*
 import org.team_alilm.domain.Basket
 import org.team_alilm.domain.Product
 import org.team_alilm.global.error.BasketAlreadyExistsException
+import org.team_alilm.global.util.StringConstant
 
 @Service
 @Transactional(readOnly = true)
@@ -13,7 +15,8 @@ class AlilmRegistrationService(
     private val loadProductPort: org.team_alilm.application.port.out.LoadProductPort,
     private val addProductPort: org.team_alilm.application.port.out.AddProductPort,
     private val loadBasketPort: org.team_alilm.application.port.out.LoadBasketPort,
-    private val addBasketPort: org.team_alilm.application.port.out.AddBasketPort
+    private val addBasketPort: org.team_alilm.application.port.out.AddBasketPort,
+    private val slackGateway: SlackGateway
 ) : org.team_alilm.application.port.`in`.use_case.AlilmRegistrationUseCase {
 
     private val log = org.slf4j.LoggerFactory.getLogger(this::class.java)
@@ -48,14 +51,11 @@ class AlilmRegistrationService(
                 it.alilmDate = null
                 it.isDelete = false
             } else {
-                // 알림을 받지도 삭제 하지도 않았다면 기다리는 상품이 있는 것 입니다.
-                log.info("장바구니가 이미 존재합니다. memberId: ${command.member.id}, productId: ${product.id}")
                 throw BasketAlreadyExistsException()
             }
 
             it
         } ?: run {
-            log.info("장바구니를 등록 합니다.")
             Basket(
                 id = Basket.BasketId(null),
                 memberId = command.member.id,
@@ -69,6 +69,15 @@ class AlilmRegistrationService(
             member = command.member,
             product = product
         )
+
+        slackGateway.sendMessage(
+            message = """
+                |알림 등록 완료
+                |회원: ${command.member.nickname}
+                |상품명: ${product.name}
+                |링크: ${StringConstant.MUSINSA_PRODUCT_HTML_REQUEST_URL.get().format(product.number)}
+            """.trimIndent()
+        )
     }
 
     private fun saveBasketV2(
@@ -80,11 +89,7 @@ class AlilmRegistrationService(
             memberId = command.member.id!!,
             productId = product.id!!
         ) ?.let {
-            /*
-                알림을 이미 받은 상품
-                삭제한 상품은
-                재 등록 시 복구 합니다.
-            */
+
             if(it.isAlilm || it.isDelete) {
                 it.isAlilm = false
                 it.alilmDate = null
@@ -111,6 +116,8 @@ class AlilmRegistrationService(
             member = command.member,
             product = product
         )
+
+
     }
 
     private fun getProductV2(command: AlilmRegistrationCommandV2) =
@@ -128,7 +135,7 @@ class AlilmRegistrationService(
                     name = command.name,
                     brand = command.brand,
                     store = command.store,
-                    imageUrl = command.imageUrl.get(0),
+                    imageUrl = command.imageUrl[0],
                     category = command.category,
                     price = command.price,
                     firstOption = command.firstOption,
