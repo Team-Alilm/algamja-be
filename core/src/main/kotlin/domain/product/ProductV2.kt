b0ef942d8d98c0576810bb227064a8d535b46e7e
cloudwi <cloudwiiiii@gmail.com>
2024-11-06 09:15:00 +0900

    feat : 상품 등록 V2

---
+++ core/src/main/kotlin/domain/product/ProductV2.kt
@@ -0,0 +1,43 @@
+package org.team_alilm.domain.product
+
+class ProductV2 (
+    val id: ProductId?,
+    val number: Long,
+    val name: String,
+    val brand: String,
+    val category: String,
+    val price: Int,
+    val store: Store,
+    val firstOption: String,
+    val secondOption: String?,
+    val thirdOption: String?
+) {
+    init {
+        require(number > 0) { "Product number must be positive" }
+        require(name.isNotBlank()) { "Product name must not be blank" }
+        require(brand.isNotBlank()) { "Product brand must not be blank" }
+        require(category.isNotBlank()) { "Product category must not be blank" }
+        require(price >= 0) { "Product price must be non-negative" }
+        require(firstOption.isNotBlank()) { "Product option1 must not be blank" }
+    }
+
+    fun getManagedCode() : String {
+        return if (this.firstOption.isNotBlank() && this.secondOption?.isNotBlank() == true && this.thirdOption?.isNotBlank() == true) {
+            "${firstOption}^${secondOption}^${thirdOption}"
+        } else if (firstOption.isNotBlank() && secondOption?.isNotBlank() == true) {
+            "${firstOption}^${secondOption}"
+        } else {
+            firstOption
+        }
+    }
+
+    fun getEmailOption() : String {
+        return if (this.firstOption.isNotBlank() && this.secondOption?.isNotBlank() == true && this.thirdOption?.isNotBlank() == true) {
+            "${this.firstOption} / ${this.secondOption} / ${this.thirdOption}"
+        } else if (this.firstOption.isNotBlank() && this.secondOption?.isNotBlank() == true) {
+            "${this.firstOption} / ${this.secondOption}"
+        } else {
+            this.firstOption
+        }
+    }
+}
