f19c8dab3e0a817d0a645c0c2b7b3e935763f34e
장주영 <jooyoung.jang@skylabs.io>
2024-11-11 09:31:18 +0900

    feat : 관련 상품 조회 api 개발했어요.

---
+++ core/src/main/kotlin/application/service/ProductRelatedService.kt
@@ -0,0 +1,25 @@
+package org.team_alilm.application.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.team_alilm.application.port.`in`.use_case.ProductRelatedUseCase
+import org.team_alilm.application.port.`in`.use_case.ProductRelatedUseCase.*
+import org.team_alilm.application.port.out.LoadProductPort
+import org.team_alilm.domain.product.ProductId
+import org.team_alilm.global.error.NotFoundProductException
+
+@Service
+@Transactional(readOnly = true)
+class ProductRelatedService(
+    private val loadProductPort: LoadProductPort
+) : ProductRelatedUseCase {
+
+    override fun productRelated(command: ProductRelatedCommand): ProductRelatedResult {
+        val product = loadProductPort.loadProduct(ProductId(command.productId)) ?: throw NotFoundProductException()
+        val productRelatedList = loadProductPort.loadRelatedProduct(product.category)
+
+        return ProductRelatedResult(
+            productList = productRelatedList
+        )
+    }
+}
