57b2bf050844516ba07c44cc18c9a5d43fb8cf63
장주영 <jooyoung.jang@skylabs.io>
2024-12-11 21:56:56 +0900

    feat: api 서버 만들었어요.

--- core/src/main/kotlin/adapter/out/gateway/MailGateway.kt
+++ core/src/main/kotlin/adapter/out/gateway/MailGateway.kt
@@ -130,9 +130,10 @@ class MailGateway(
               <div style="width: 100%; height: 28px;"></div>
               <div style="color= #ffffff; display: flex; justify-content: center; align-items: center; width: 100%; height: 44px; background-color: #1B1A3B; border-radius: 8px;" >
                 <a href="https://www.musinsa.com/app/goods/${productNumber}" 
-                   style="background-color: #1B1A3B; color: inherit; text-decoration: none;">
+                   style="background-color: #1B1A3B; color: #ffffff; text-decoration: none;">
                    재입고 상품 구매하러 가기 👉
                 </a>
+
               </div>
             </div>
             </body>
