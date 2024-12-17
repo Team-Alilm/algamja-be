eb9ef5b571f0fa3f7241ad2507f1a8f86a405750
cloudwi <cloudwiiiii@gmail.com>
2024-12-17 17:35:11 +0900

    feat: slack message add

--- core/src/main/kotlin/global/util/StringConstant.kt
+++ core/src/main/kotlin/global/util/StringConstant.kt
@@ -7,8 +7,8 @@ enum class StringConstant(
     MUSINSA_OPTION_API_URL("https://goods-detail.musinsa.com/api2/goods/%s/options?goodsSaleType=SALE"),
     MUSINSA_PRODUCT_HTML_URL("https://store.musinsa.com/app/goods/%s"),
 
-    ABLY_ANONYMOUS_TOKEN("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbm9ueW1vdXNfaWQiOiIzMTk0MzE3NjYiLCJpYXQiOjE3MzE4ODUwNjl9.iMCkiNw50N05BAatKxnvMYWAg_B7gBUiBL6FZe1Og9Y");
-
+    ABLY_ANONYMOUS_TOKEN("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbm9ueW1vdXNfaWQiOiIzMTk0MzE3NjYiLCJpYXQiOjE3MzE4ODUwNjl9.iMCkiNw50N05BAatKxnvMYWAg_B7gBUiBL6FZe1Og9Y"),
+    ABLY_PRODUCT_OPTIONS_API_URL("https://api.a-bly.com/api/v2/goods/%s/options/?depth=1");
     fun get(): String {
         return value
     }
--- worker/src/main/kotlin/quartz/job/handler/impl/ABlyHandler.kt
+++ worker/src/main/kotlin/quartz/job/handler/impl/ABlyHandler.kt
@@ -1,7 +1,11 @@
 package org.team_alilm.quartz.job.handler.impl
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClientException
+import org.springframework.web.client.RestTemplate
+import org.team_alilm.adapter.out.gateway.FcmSendGateway
+import org.team_alilm.adapter.out.gateway.MailGateway
 import org.team_alilm.application.port.out.gateway.crawling.CrawlingGateway
 import org.team_alilm.domain.product.Product
 import org.team_alilm.global.util.StringConstant
@@ -9,7 +13,12 @@ import org.team_alilm.quartz.job.handler.PlatformHandler
 
 
 @Component
-class ABlyHandler : PlatformHandler {
+class ABlyHandler(
+    private val sendSlackGateway: CrawlingGateway,
+    private val restTemplate: RestTemplate,
+    private val mailGateway: MailGateway,
+    private val fcmSendGateway: FcmSendGateway
+) : PlatformHandler {
 
     override fun process(product: Product) {
         if(checkSoldOut(product).not()) {
@@ -18,10 +27,13 @@ class ABlyHandler : PlatformHandler {
     }
 
     private fun checkSoldOut(product: Product): Boolean {
-        val musinsaProductHtmlRequestUrl = StringConstant.MUSINSA_PRODUCT_HTML_URL.get().format(product.number)
-        val crawlingGatewayRequest = CrawlingGateway.CrawlingGatewayRequest(musinsaProductHtmlRequestUrl)
-        val response = crawlingGateway.htmlCrawling(crawlingGatewayRequest)
-        val jsonData = extractJsonData(response.html)
+        val ablyAnonymousToken = StringConstant.ABLY_ANONYMOUS_TOKEN
+        val ablyProductOptions = restTemplate.getForObject(
+            StringConstant.ABLY_PRODUCT_OPTIONS_API_URL.get().format(product.number),
+            Option::class.java
+        )
+
+
 
         return if (jsonData != null) {
             // JSON 데이터 파싱
@@ -54,4 +66,24 @@ class ABlyHandler : PlatformHandler {
             }
         }
     }
+
+    data class Option(
+        val sno: Long,
+        val depth: Int,
+        val name: String,
+        val is_final_depth: Boolean,
+        val goods_option_sno: Long,
+        val delivery_type: String,
+        val standard_delivery_message: String,
+        val delivery_lead_days: Any?, // null 가능
+        val goods_option: Any?, // null 가능
+        val wholesale_name: Any?, // null 가능
+        val has_high_demand_tag: Boolean
+    )
+
+    data class Color(
+        val name: String,
+        val option_components: List<Option>
+    )
+
 }
