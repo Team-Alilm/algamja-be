dbdaacef27e38b8722f757444a956ca575493993
cloudwi <cloudwiiiii@gmail.com>
2024-12-23 08:34:00 +0900

    feat: ably images add

--- core/src/main/kotlin/application/service/AblyProductCrawlingService.kt
+++ core/src/main/kotlin/application/service/AblyProductCrawlingService.kt
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.team_alilm.application.port.`in`.use_case.product.crawling.ProductCrawlingUseCase
 import org.team_alilm.domain.product.Store
+import org.team_alilm.global.util.StringConstant
 
 @Service
 class AblyProductCrawlingService(
@@ -18,19 +19,19 @@ class AblyProductCrawlingService(
     override fun crawling(command: ProductCrawlingUseCase.ProductCrawlingCommand): ProductCrawlingUseCase.CrawlingResult {
         val productNumber = getProductNumber(command.url)
         val headers = org.springframework.http.HttpHeaders().apply {
-            add("X-Anonymous-Token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbm9ueW1vdXNfaWQiOiIzMTk0MzE3NjYiLCJpYXQiOjE3MzE4ODUwNjl9.iMCkiNw50N05BAatKxnvMYWAg_B7gBUiBL6FZe1Og9Y") // Authorization 헤더
+            add("X-Anonymous-Token", StringConstant.ABLY_ANONYMOUS_TOKEN.get()) // Authorization 헤더
         }
         val entity = HttpEntity<String>(headers)
 
         val response = restTemplate.exchange(
-            "https://api.a-bly.com/api/v2/goods/$productNumber",
+            StringConstant.ABLY_PRODUCT_API_URL.get().format(productNumber), // URL
             HttpMethod.GET,     // HTTP 메서드
             entity,             // HttpEntity (헤더 포함)
             JsonNode::class.java  // 응답 타입
         ).body
 
         val firstOptions = restTemplate.exchange(
-            "https://api.a-bly.com/api/v2/goods/$productNumber/options/?depth=1",
+            StringConstant.ABLY_PRODUCT_OPTIONS_API_URL.get().format(productNumber, 1),
             HttpMethod.GET,
             entity,
             JsonNode::class.java
@@ -39,7 +40,7 @@ class AblyProductCrawlingService(
         val secondOptions = try {
             if (firstOptions?.get("option_components")?.first()?.isEmpty?.not() == true) {
                 restTemplate.exchange(
-                    "https://api.a-bly.com/api/v2/goods/$productNumber/options/?depth=2&selected_option_sno=${
+                    StringConstant.ABLY_PRODUCT_OPTIONS_API_URL.get().format(productNumber, 2) + "&selected_option_sno=${
                         firstOptions.get("option_components")
                             ?.first()?.get("goods_option_sno")
                     }",
@@ -58,8 +59,8 @@ class AblyProductCrawlingService(
         val thirdOptions = try {
             if (secondOptions?.get("option_components")?.first()?.isEmpty?.not() == true) {
                 restTemplate.exchange(
-                    "https://api.a-bly.com/api/v2/goods/$productNumber/options/?depth=3&selected_option_sno=${
-                        secondOptions.get("option_components")
+                    StringConstant.ABLY_PRODUCT_OPTIONS_API_URL.get().format(productNumber, 3) + "&selected_option_sno=${
+                        firstOptions?.get("option_components")
                             ?.first()?.get("goods_option_sno")
                     }",
                     HttpMethod.GET,
@@ -70,17 +71,35 @@ class AblyProductCrawlingService(
                 null
             }
         } catch (e: Exception) {
-            log.error("Error while fetching third options: ${e.message}")
+            log.error("""
+                |Error while fetching third options: ${e.message}
+                |https://api.a-bly.com/api/v2/goods/$productNumber/options/?depth=1
+            """.trimMargin())
             null
         }
 
+        val relatedGoodsApiRequestUrl = StringConstant.ABLY_PRODUCT_RELATED_GOODS_API_URL.get().format(productNumber)
+        val relatedGoodsResponse = restTemplate.exchange(
+            relatedGoodsApiRequestUrl,
+            HttpMethod.GET,
+            entity,
+            JsonNode::class.java
+        ).body
+
+        val imageUrlList = relatedGoodsResponse
+            ?.get("item_list")
+            ?.first { it.get("type")?.asText() == "image_similar_goods" }
+            ?.get("goods")
+            ?.map { it.get("image").asText() }
+            ?.take(3) // 상위 3개만 추출
+            ?: emptyList()
 
         return ProductCrawlingUseCase.CrawlingResult(
             number = response?.get("goods")?.get("sno")?.asLong() ?: throw IllegalArgumentException("상품 정보를 가져올 수 없습니다."),
             name = response.get("goods")?.get("name")?.asText() ?: throw IllegalArgumentException("상품 정보를 가져올 수 없습니다."),
             brand = response.get("goods")?.get("market")?.get("name")?.asText() ?: throw IllegalArgumentException("상품 정보를 가져올 수 없습니다."),
             thumbnailUrl = response.get("goods")?.get("first_page_rendering")?.get("cover_image")?.asText() ?: throw IllegalArgumentException("상품 정보를 가져올 수 없습니다."),
-            imageUrlList = emptyList(),
+            imageUrlList = imageUrlList,
             firstCategory = response.get("goods")?.get("category")?.get("name")?.asText() ?: throw IllegalArgumentException("상품 정보를 가져올 수 없습니다."),
             secondCategory = null,
             price = response.get("goods")?.get("first_page_rendering")?.get("original_price")?.asInt() ?: throw IllegalArgumentException("상품 정보를 가져올 수 없습니다."),
