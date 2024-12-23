d1b40f16b87a291488e957e490dc8f621b2caa90
장주영 <jooyoung.jang@skylabs.io>
2024-12-23 17:01:39 +0900

    feat: 이미지 저장 로직 개선 중 입니다.

--- core/src/main/kotlin/adapter/out/persistence/adapter/ProductImageAdapter.kt
+++ core/src/main/kotlin/adapter/out/persistence/adapter/ProductImageAdapter.kt
@@ -1,16 +1,19 @@
 package org.team_alilm.adapter.out.persistence.adapter
 
 import org.springframework.stereotype.Component
-import org.team_alilm.adapter.out.persistence.repository.spring_data.SpringDataProductImageRepository
-import org.team_alilm.application.port.out.LoadProductImagePort
-import org.team_alilm.domain.product.Store
+import org.team_alilm.adapter.out.persistence.mapper.ProductImageMapper
+import org.team_alilm.adapter.out.persistence.repository.ProductImageRepository
+import org.team_alilm.application.port.out.AddProductImagePort
+import org.team_alilm.domain.product.ProductImage
 
 @Component
 class ProductImageAdapter(
-    private val springDataProductImageRepository: SpringDataProductImageRepository
-) : LoadProductImagePort {
+    private val productImageRepository: ProductImageRepository,
+    private val productImageMapper: ProductImageMapper
+) : AddProductImagePort {
 
-    override fun existsByProductImage(productNumber: Long, productStore: Store): Boolean {
-        return springDataProductImageRepository.existsByProductNumberAndProductStore(productNumber, productStore)
+    override fun add(productImages: List<ProductImage>) {
+        val productImageJpaEntitys = productImages.map { productImageMapper.mapToJpaEntity(it) }
+        productImageRepository.saveAllIgnore(productImageJpaEntitys)
     }
 }
