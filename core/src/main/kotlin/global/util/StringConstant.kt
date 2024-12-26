e6559fc028cee8d473b3de7abc7f99b0eeb7ed05
cloudwi <cloudwiiiii@gmail.com>
2024-12-26 16:14:14 +0900

    feat: ably token reset

--- core/src/main/kotlin/global/util/StringConstant.kt
+++ core/src/main/kotlin/global/util/StringConstant.kt
@@ -9,7 +9,7 @@ enum class StringConstant(
     MUSINSA_PRODUCT_IMAGES_URL("https://goods-detail.musinsa.com/api2/goods/%s/recommends/multi?uuid=detail_goods_attributes_allbrand&limit=3"),
 
     ABLY_PRODUCT_API_URL("https://api.a-bly.com/api/v2/goods/%s"),
-    ABLY_ANONYMOUS_TOKEN("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbm9ueW1vdXNfaWQiOiIzMjAxNjQ2MzkiLCJpYXQiOjE3MzIwMTg4Mzh9.BdsnayAvbtVgEOoA9zyZm0cWQY3Voliqw1WeO9iyHqw"),
+    ABLY_ANONYMOUS_TOKEN("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbm9ueW1vdXNfaWQiOiIzMTk0MzE3NjYiLCJpYXQiOjE3MzE4ODUwNjl9.iMCkiNw50N05BAatKxnvMYWAg_B7gBUiBL6FZe1Og9Y"),
     ABLY_PRODUCT_OPTIONS_API_URL("https://api.a-bly.com/api/v2/goods/%s/options/?depth=%s"),
     ABLY_PRODUCT_RELATED_GOODS_API_URL("https://api.a-bly.com/api/v2/goods/%s/related_goods"),;
 
