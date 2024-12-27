ca2335c37e5b644cdaaaa5199b780390754d27e6
장주영 <jooyoung.jang@skylabs.io>
2024-12-27 13:30:54 +0900

    feat: aNonymousToken add

--- core/src/main/kotlin/global/util/StringContextHolder.kt
+++ core/src/main/kotlin/global/util/StringContextHolder.kt
@@ -9,7 +9,7 @@ enum class StringContextHolder(
     MUSINSA_PRODUCT_IMAGES_URL("https://goods-detail.musinsa.com/api2/goods/%s/recommends/multi?uuid=detail_goods_attributes_allbrand&limit=3"),
 
     ABLY_PRODUCT_API_URL("https://api.a-bly.com/api/v2/goods/%s/?channel=0"),
-    ABLY_ANONYMOUS_TOKEN("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhbm9ueW1vdXNfaWQiOiIzMjAxNjQ2MzkiLCJpYXQiOjE3MzIwMTg4Mzh9.BdsnayAvbtVgEOoA9zyZm0cWQY3Voliqw1WeO9iyHqw"),
+    ABLY_ANONYMOUS_TOKEN_API_URL("https://api.a-bly.com/api/v2/anonymous/token/"),
     ABLY_PRODUCT_OPTIONS_API_URL("https://api.a-bly.com/api/v2/goods/%s/options/?depth=%s"),
     ABLY_PRODUCT_RELATED_GOODS_API_URL("https://api.a-bly.com/api/v2/goods/%s/related_goods"),
 
