8d8fbf312c74eb4f9c1f6938b7bbb29c89ba1559
cloudwi <cloudwiiiii@gmail.com>
2025-01-06 13:30:40 +0900

    feat: 스크립트 추가

--- worker/src/main/kotlin/quartz/job/SoldoutCheckJob.kt
+++ worker/src/main/kotlin/quartz/job/SoldoutCheckJob.kt
@@ -16,8 +16,6 @@ class SoldoutCheckJob(
     private val sqsTemplate: SqsTemplate
 ) : Job {
 
-    private val log = LoggerFactory.getLogger(SoldoutCheckJob::class.java)
-
     @Transactional
     override fun execute(context: JobExecutionContext) {
         val productList: List<Product> = loadCrawlingProductsPort.loadCrawlingProducts()
@@ -25,28 +23,5 @@ class SoldoutCheckJob(
         productList.forEach {
             sqsTemplate.send("product-soldout-check-queue", it)
         }
-
-        // 비동기 작업으로 전환해요.
-//        coroutineScope.launch {
-//            productList.chunked(10).forEach { chunk ->
-//                launch {
-//                    chunk.forEach { product ->
-//                        try {
-//                            val soldoutProduct = platformHandlerResolver
-//                                .resolve(product.store)
-//                                .process(product)
-//
-//                            if (soldoutProduct != null) {
-//                                // 상품이 품절이 아닌 경우, 저장해요.
-//                                sqsTemplate.send("AlilmQueue", product)
-//                            }
-//                        } catch (e: Exception) {
-//                            log.info("Error processing product ${product.id}: ${e.message}")
-//                            slackGateway.sendMessage("Error processing productId: ${product.id}: ${e.message}")
-//                        }
-//                    }
-//                }
-//            }
-//        }
     }
 }
