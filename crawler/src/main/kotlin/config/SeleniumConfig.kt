7c655ccd3cd76fd3903894317b462312b4b4999e
cloudwi <cloudwiiiii@gmail.com>
2025-01-08 15:33:25 +0900

    fix: 에이블리 api 추가

--- crawler/src/main/kotlin/config/SeleniumConfig.kt
+++ crawler/src/main/kotlin/config/SeleniumConfig.kt
@@ -13,12 +13,12 @@ class SeleniumConfig {
     @Bean
     fun webDriver(): WebDriver {
         val options = ChromeOptions()
-        options.addArguments("window-size=1920,1080")
-        options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
+        options.addArguments("--no-sandbox")
+        options.addArguments("--disable-dev-shm-usage")
         options.addArguments("--disable-gpu")
-        options.setExperimentalOption("excludeSwitches", listOf("enable-automation"))
-        options.setExperimentalOption("useAutomationExtension", false)
-        options.addArguments("user-data-dir=/path/to/your/chrome/profile") // 기존 브라우저 프로필 사용
+        options.addArguments("--window-size=1920,1080")
+        options.addArguments("--ignore-certificate-errors")
+
         options.setAcceptInsecureCerts(true)
 
         val driver = ChromeDriver(options)
