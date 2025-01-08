f5049089fed3bf8291559f56be2d77d26c3bee32
cloudwi <cloudwiiiii@gmail.com>
2025-01-08 16:57:15 +0900

    fix: 에이블리 api 추가

--- crawler/src/main/kotlin/config/SeleniumConfig.kt
+++ crawler/src/main/kotlin/config/SeleniumConfig.kt
@@ -16,14 +16,16 @@ class SeleniumConfig {
         WebDriverManager.chromedriver().setup()  // WebDriverManager로 ChromeDriver 설정
 
         val options = ChromeOptions()
-
-        // 크롬 설치 경로를 명시적으로 설정
+        options.addArguments("--headless")  // 헤드리스 모드 활성화
         options.addArguments("--no-sandbox")
         options.addArguments("--disable-dev-shm-usage")
         options.addArguments("--disable-gpu")
         options.addArguments("--window-size=1920,1080")
         options.addArguments("--ignore-certificate-errors")
         options.setAcceptInsecureCerts(true)
+
+        // User-Agent 변경
+        options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
         val driver = ChromeDriver(options)
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1))
