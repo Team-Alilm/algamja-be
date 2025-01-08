b7e0778255fad09e590e9ede9c21de62bb8b71c0
cloudwi <cloudwiiiii@gmail.com>
2025-01-08 15:19:18 +0900

    fix: 에이블리 api 추가

--- crawler/src/main/kotlin/config/SeleniumConfig.kt
+++ crawler/src/main/kotlin/config/SeleniumConfig.kt
@@ -12,8 +12,6 @@ class SeleniumConfig {
 
     @Bean
     fun webDriver(): WebDriver {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver")
-
         val options = ChromeOptions()
         options.addArguments("window-size=1920,1080")
         options.addArguments("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36")
