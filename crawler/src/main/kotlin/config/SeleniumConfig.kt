66ee0ea734884c1a5d87ebd94d3566063999ebb9
장주영 <jooyoung.jang@skylabs.io>
2025-01-09 16:49:15 +0900

    fix: 에이블리 api 추가

--- crawler/src/main/kotlin/config/SeleniumConfig.kt
+++ crawler/src/main/kotlin/config/SeleniumConfig.kt
@@ -13,14 +13,15 @@ class SeleniumConfig {
 
     @Bean
     fun webDriver(): WebDriver {
+        WebDriverManager.chromedriver().setup()  // WebDriverManager로 ChromeDriver 설정
+
+        //ec2에서도 돌아가야함
         val options = ChromeOptions()
         options.addArguments("--headless")
         options.addArguments("--disable-gpu")
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
         val driver = ChromeDriver(options)
-
-        WebDriverManager.chromedriver().setup()  // WebDriverManager로 ChromeDriver 설정
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1))
 
