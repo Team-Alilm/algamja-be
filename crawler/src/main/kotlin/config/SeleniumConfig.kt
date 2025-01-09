29811d278149e98fcef18e81235c4fe44ed806d9
cloudwi <cloudwiiiii@gmail.com>
2025-01-10 08:26:55 +0900

    fix: 에이블리 api 추가

--- crawler/src/main/kotlin/config/SeleniumConfig.kt
+++ crawler/src/main/kotlin/config/SeleniumConfig.kt
@@ -5,14 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.Duration
 
 @Configuration
 class SeleniumConfig {
 
     @Bean
     fun webDriver(): WebDriver {
-        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+
         //ec2에서도 돌아가야함
         val options = ChromeOptions()
         options.addArguments("--headless")
@@ -20,8 +19,6 @@ class SeleniumConfig {
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
         val driver = ChromeDriver(options)
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1))
 
         return driver
     }
