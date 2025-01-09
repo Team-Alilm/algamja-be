107e848aa925c26d577405c3ea350634fc6e99cd
장주영 <jooyoung.jang@skylabs.io>
2025-01-09 14:08:25 +0900

    fix: 에이블리 api 추가

--- crawler/src/main/kotlin/config/SeleniumConfig.kt
+++ crawler/src/main/kotlin/config/SeleniumConfig.kt
@@ -14,15 +14,13 @@ class SeleniumConfig {
     @Bean
     fun webDriver(): WebDriver {
         val options = ChromeOptions()
-        options.addArguments("--headless")  // 헤드리스 모드 활성화
+        options.addArguments("--headless=new");
         options.addArguments("--no-sandbox")
         options.addArguments("--disable-dev-shm-usage")
         options.addArguments("--disable-gpu")
         options.addArguments("--window-size=1920,1080")
         options.addArguments("--ignore-certificate-errors")
         options.setAcceptInsecureCerts(true)
-
-        // User-Agent 변경
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 
         val driver = ChromeDriver(options)
