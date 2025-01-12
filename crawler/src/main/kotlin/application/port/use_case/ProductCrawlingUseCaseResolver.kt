f15f5fe57f41a4112da3509933af97f02d4a120d
jangjuyeong <cloudwi@naver.com>
2025-01-12 21:20:28 +0900

    feat: hotfix

--- crawler/src/main/kotlin/application/port/use_case/ProductCrawlingUseCaseResolver.kt
+++ crawler/src/main/kotlin/application/port/use_case/ProductCrawlingUseCaseResolver.kt
@@ -2,14 +2,14 @@ package org.team_alilm.application.port.use_case
 
 import domain.product.Store
 import org.springframework.stereotype.Component
-import org.team_alilm.application.service.AblyProductCrawlingService
+//import org.team_alilm.application.service.AblyProductCrawlingService
 import org.team_alilm.application.service.CM29ProductCrawlingService
 import org.team_alilm.application.service.MusinsaProductCrawlingService
 
 @Component
 class ProductCrawlingUseCaseResolver(
     private val muSinSaProductCrawlingUseCase: MusinsaProductCrawlingService,
-    private val aBlyProductCrawlingUseCase: AblyProductCrawlingService,
+//    private val aBlyProductCrawlingUseCase: AblyProductCrawlingService,
     private val cm29ProductCrawlingUseCase: CM29ProductCrawlingService,
 ) {
 
@@ -17,7 +17,7 @@ class ProductCrawlingUseCaseResolver(
         return when (store) {
             Store.CM29 -> cm29ProductCrawlingUseCase
             Store.MUSINSA -> muSinSaProductCrawlingUseCase
-            Store.A_BLY -> aBlyProductCrawlingUseCase
+//            Store.A_BLY -> aBlyProductCrawlingUseCase
             Store.NONE -> throw IllegalArgumentException("Unknown store")
         }
     }
