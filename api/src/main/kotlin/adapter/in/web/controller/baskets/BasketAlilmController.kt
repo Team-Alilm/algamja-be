534fc10f55abbd469f44804ea5f8ed211b095797
cloudwi <cloudwiiiii@gmail.com>
2025-01-13 11:03:49 +0900

    fix:

--- api/src/main/kotlin/adapter/in/web/controller/baskets/BasketAlilmController.kt
+++ api/src/main/kotlin/adapter/in/web/controller/baskets/BasketAlilmController.kt
@@ -1,7 +1,6 @@
 package org.team_alilm.adapter.`in`.web.controller.baskets
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import domain.product.ProductId
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PutMapping
