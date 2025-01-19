463b10591f24f596fdbdea8539da3c6f063c41a3
HYUNAHSHIM <hyunahshim@naver.com>
2025-01-19 20:44:49 +0900

    feat: 알림 읽음처리 API 개발

--- core/src/main/kotlin/application/port/out/LoadAlilmPort.kt
+++ core/src/main/kotlin/application/port/out/LoadAlilmPort.kt
@@ -1,8 +1,10 @@
 package org.team_alilm.application.port.out
 
+import domain.Alilm
 import domain.product.Product
 
 interface LoadAlilmPort {
 
     fun loadAlilm(count: Int) : List<Product>
+    fun loadAlilmById(alilmId: Long) : Alilm
 }
