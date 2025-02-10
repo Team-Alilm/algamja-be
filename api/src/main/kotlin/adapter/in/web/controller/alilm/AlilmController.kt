a86e7a4f3fe85b073ac641a0f1401bde1fdb8c01
cloudwi <cloudwiiiii@gmail.com>
2025-02-10 14:46:49 +0900

    hotfix: recent 사용하지 않아서 제거

--- api/src/main/kotlin/adapter/in/web/controller/alilm/AlilmController.kt
+++ api/src/main/kotlin/adapter/in/web/controller/alilm/AlilmController.kt
@@ -1,6 +1,5 @@
 package org.team_alilm.adapter.`in`.web.controller.alilm
 
-import domain.product.Product
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.team_alilm.application.port.`in`.use_case.AlilmHistoryUseCase
-import org.team_alilm.application.port.`in`.use_case.AlilmRecentUseCase
 
 
 @RestController
@@ -17,44 +15,8 @@ import org.team_alilm.application.port.`in`.use_case.AlilmRecentUseCase
     알림 API 들을 모아둔 컨트롤러 입니다.
 """)
 class AlilmController(
-    private val alilmRecentUseCase: AlilmRecentUseCase,
     private val alilmHistoryUseCase: AlilmHistoryUseCase
 ) {
-
-    @Operation(
-        summary = "최근 알림 조회 API",
-        description = """
-            최근 일주일 이내에 재 입고 상품 목록을 반환합니다.
-            최대 12개 입니다.
-    """)
-    @GetMapping("/recent")
-    fun alilmRecent(): ResponseEntity<AlilmRecentResponse> {
-        val result = alilmRecentUseCase.alilmRecent()
-        val productList = result.productList.map { RecentProduct.from(it) }
-
-        val response = AlilmRecentResponse(productList = productList)
-        return ResponseEntity.ok(response)
-    }
-
-    data class AlilmRecentResponse(
-        val productList: List<RecentProduct>
-    )
-
-    data class RecentProduct(
-        val id: Long,
-        val name: String,
-        val imageUrl: String,
-    ) {
-        companion object {
-            fun from(product: Product): RecentProduct {
-                return RecentProduct(
-                    id = product.id?.value ?: 0,
-                    name = product.name,
-                    imageUrl = product.thumbnailUrl
-                )
-            }
-        }
-    }
 
     @Operation(
         summary = "알림 기록 조회 API",
