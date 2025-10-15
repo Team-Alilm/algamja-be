ffb8762c7e09d551aa384e312e00b297c471d059
jangjuyeong.jang <cloudwiiiii@gmail.com>
2025-10-15 13:05:35 +0900

    fix: 무신사 크롤링 시 403 에러 방지를 위해 브라우저 헤더 개선
    - User-Agent를 실제 Chrome 브라우저와 동일하게 변경
    - Sec-Ch-Ua, Sec-Fetch-* 등 브라우저 보안 헤더 추가
    - Accept, Accept-Language, Accept-Encoding 헤더 추가
    - RestClient와 Jsoup 모두에 동일한 헤더 적용

--- src/main/kotlin/org/team_alilm/algamja/common/config/RestClientConfig.kt
+++ src/main/kotlin/org/team_alilm/algamja/common/config/RestClientConfig.kt
@@ -15,7 +15,18 @@ class RestClientConfig {
     @Bean
     fun restClient(): RestClient =
         RestClient.builder()
-            .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (compatible; AlilmBot/1.0)")
+            .defaultHeader(HttpHeaders.USER_AGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+            .defaultHeader(HttpHeaders.ACCEPT, "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8")
+            .defaultHeader(HttpHeaders.ACCEPT_LANGUAGE, "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7")
+            .defaultHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br")
+            .defaultHeader("Sec-Ch-Ua", "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"")
+            .defaultHeader("Sec-Ch-Ua-Mobile", "?0")
+            .defaultHeader("Sec-Ch-Ua-Platform", "\"Windows\"")
+            .defaultHeader("Sec-Fetch-Dest", "document")
+            .defaultHeader("Sec-Fetch-Mode", "navigate")
+            .defaultHeader("Sec-Fetch-Site", "none")
+            .defaultHeader("Sec-Fetch-User", "?1")
+            .defaultHeader("Upgrade-Insecure-Requests", "1")
             .requestFactory(simpleClientHttpRequestFactory())
             .requestInterceptor { req, body, exec ->
                 if (log.isDebugEnabled) {
